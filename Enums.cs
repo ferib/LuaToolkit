8cdf01488117fdc6b2d38d2a6e504bd6cc51a15a
= <lopke123@live.nl>
2021-02-07 19:41:19 +0100

    add some handlers

--- Enums.cs
+++ Enums.cs
@@ -19,7 +19,7 @@ namespace LuaSharpVM
 
     //}
 
-    public enum Opcode
+    public enum OpcodeName
     {
         MOVE = 0,
         LOADK,
@@ -60,4 +60,40 @@ namespace LuaSharpVM
         CLOSURE,
         VARARG
     }
+    public enum OpcodeType
+    {
+        ABC,
+        ABx,
+        AsBx
+    }
+
+    public struct LuaInstruction
+    {
+        public OpcodeType Opcode;
+        public int Type; // ABC, ABx, AsBx
+        public int A;
+        public int B;
+        public int Bx;
+        public int C;
+        public int sBx;
+    }
+
+    public class LuaRegisters
+    {
+        public byte A;  // 8 bits
+        public ushort B;  // 9 bits
+        public ushort C;  // 9 buts
+        public int Ax  // 26 nits (A, B and C)
+        {
+            get { return A + B + C; }
+            //set { A = value; }
+        }
+        public int Bx;  // 18 bits (B and C)
+        public int sBx // signed Bx
+        {
+            get { return (short)Bx; }
+            set { Bx = value; }
+        }
+    }
+
 }
--- LuaVM.cs
+++ LuaVM.cs
@@ -3,126 +3,176 @@ using System.Collections.Generic;
 
 namespace LuaSharpVM
 {
-    public struct LuaInstruction
+    public class Chunk
     {
-        public Opcode Opcode;
-        public int A;
-        public int B;
-        public int Bx;
-        public int C;
-        public int sBx;
+        public string Name;
+        public int FirstLine;
+        public int LastLine;
+        public byte Upvalues;
+        public byte Arguments;
+        public byte Vargs;
+        public byte Stack;
+        public List<LuaInstruction> Instructions;
+        public List<string> Constants;
+        public List<string> Prototypes;
+        public List<string> DebugLines;
+
+        public Chunk()
+        {
+            this.Instructions = new List<LuaInstruction>();
+            this.Constants = new List<String>();
+            this.Prototypes = new List<String>();
+            this.DebugLines = new List<String>();
+        }
     }
 
     public class LuaVM
     {
         private byte[] Buffer;
-        private int Index;
-        private Dictionary<Opcode, Action<LuaInstruction>> InstructionTable;
+        private int IP;
+        private Chunk Chunk;
+        private LuaRegisters Registers;
+        private Dictionary<int, object> Stack;
+        private Dictionary<int, object> Constants;
+        private Dictionary<int, object> Upvalues;
+        private Dictionary<int, object> Environment;
+        private Dictionary<OpcodeName, Action<LuaInstruction>> InstructionTable;
+        
 
         public LuaVM(byte[] LuaC)
         {
             this.Buffer = LuaC;
-            this.Index = 0;
-            this.InstructionTable = new Dictionary<Opcode, Action<LuaInstruction>>()
+            this.IP = 0;
+            this.Chunk = new Chunk();
+            this.Registers = new LuaRegisters();
+            this.Stack = new Dictionary<int, object>();
+            this.Constants = new Dictionary<int, object>();
+            this.Upvalues = new Dictionary<int, object>();
+            this.Environment = new Dictionary<int, object>();
+            this.InstructionTable = new Dictionary<OpcodeName, Action<LuaInstruction>>()
             {
-                {Opcode.MOVE, (instr) => {MOVE(instr); } },
-                {Opcode.LOADK, (instr) => {LOADK(instr); } },
-                {Opcode.LOADBOOL, (instr) => {LOADBOOL(instr); } },
-                {Opcode.LOADNIL, (instr) => {LOADNIL(instr); } },
-                {Opcode.GETUPVAL, (instr) => {GETUPVAL(instr); } },
-                {Opcode.GETGLOBAL, (instr) => {GETGLOBAL(instr); } },
-                {Opcode.GETTABLE, (instr) => {GETTABLE(instr); } },
-                {Opcode.SETGLOBAL, (instr) => {SETGLOBAL(instr); } },
-                {Opcode.SETUPVAL, (instr) => {SETUPVAL(instr); } },
-                {Opcode.SETTABLE, (instr) => {SETTABLE(instr); } },
-                {Opcode.NEWTABLE, (instr) => {NEWTABLE(instr); } },
-                {Opcode.SELF, (instr) => {SELF(instr); } },
-                {Opcode.ADD, (instr) => {ADD(instr); } },
-                {Opcode.SUB, (instr) => {SUB(instr); } },
-                {Opcode.DIV, (instr) => {DIV(instr); } },
-                {Opcode.MOD, (instr) => {MOD(instr); } },
-                {Opcode.POW, (instr) => {POW(instr); } },
-                {Opcode.UNM, (instr) => {UNM(instr); } },
-                {Opcode.NOT, (instr) => {NOT(instr); } },
-                {Opcode.LEN, (instr) => {LEN(instr); } },
-                {Opcode.CONCAT, (instr) => {CONCAT(instr); } },
-                {Opcode.JMP, (instr) => {JUMP(instr); } },
-                {Opcode.EQ, (instr) => {EQ(instr); } },
-                {Opcode.LT, (instr) => {LT(instr); } },
-                {Opcode.LE, (instr) => {LE(instr); } },
-                {Opcode.TEST, (instr) => {TEST(instr); } },
-                {Opcode.TESTSET, (instr) => {TESTSET(instr); } },
-                {Opcode.CALL, (instr) => {CALL(instr); } },
-                {Opcode.TAILCALL, (instr) => {TAILCALL(instr); } },
-                {Opcode.RETURN, (instr) => {RETURN(instr); } },
-                {Opcode.FORLOOP, (instr) => {FORLOOP(instr); } },
-                {Opcode.TFORLOOP, (instr) => {TFORLOOP(instr); } },
-                {Opcode.SETLIST, (instr) => {SETLIST(instr); } },
-                {Opcode.CLOSE, (instr) => {CLOSE(instr); } },
-                {Opcode.CLOSURE, (instr) => {CLOSURE(instr); } },
-                {Opcode.VARARG, (instr) => {VARARG(instr); } },
+                {OpcodeName.MOVE, (instr) => {MOVE(instr); } },
+                {OpcodeName.LOADK, (instr) => {LOADK(instr); } },
+                {OpcodeName.LOADBOOL, (instr) => {LOADBOOL(instr); } },
+                {OpcodeName.LOADNIL, (instr) => {LOADNIL(instr); } },
+                {OpcodeName.GETUPVAL, (instr) => {GETUPVAL(instr); } },
+                {OpcodeName.GETGLOBAL, (instr) => {GETGLOBAL(instr); } },
+                {OpcodeName.GETTABLE, (instr) => {GETTABLE(instr); } },
+                {OpcodeName.SETGLOBAL, (instr) => {SETGLOBAL(instr); } },
+                {OpcodeName.SETUPVAL, (instr) => {SETUPVAL(instr); } },
+                {OpcodeName.SETTABLE, (instr) => {SETTABLE(instr); } },
+                {OpcodeName.NEWTABLE, (instr) => {NEWTABLE(instr); } },
+                {OpcodeName.SELF, (instr) => {SELF(instr); } },
+                {OpcodeName.ADD, (instr) => {ADD(instr); } },
+                {OpcodeName.SUB, (instr) => {SUB(instr); } },
+                {OpcodeName.MUL, (instr) => {MUL(instr); } },
+                {OpcodeName.DIV, (instr) => {DIV(instr); } },
+                {OpcodeName.MOD, (instr) => {MOD(instr); } },
+                {OpcodeName.POW, (instr) => {POW(instr); } },
+                {OpcodeName.UNM, (instr) => {UNM(instr); } },
+                {OpcodeName.NOT, (instr) => {NOT(instr); } },
+                {OpcodeName.LEN, (instr) => {LEN(instr); } },
+                {OpcodeName.CONCAT, (instr) => {CONCAT(instr); } },
+                {OpcodeName.JMP, (instr) => {JUMP(instr); } },
+                {OpcodeName.EQ, (instr) => {EQ(instr); } },
+                {OpcodeName.LT, (instr) => {LT(instr); } },
+                {OpcodeName.LE, (instr) => {LE(instr); } },
+                {OpcodeName.TEST, (instr) => {TEST(instr); } },
+                {OpcodeName.TESTSET, (instr) => {TESTSET(instr); } },
+                {OpcodeName.CALL, (instr) => {CALL(instr); } },
+                {OpcodeName.TAILCALL, (instr) => {TAILCALL(instr); } },
+                {OpcodeName.RETURN, (instr) => {RETURN(instr); } },
+                {OpcodeName.FORLOOP, (instr) => {FORLOOP(instr); } },
+                {OpcodeName.FORPREP, (instr) => {FORPREP(instr); } },
+                {OpcodeName.TFORLOOP, (instr) => {TFORLOOP(instr); } },
+                {OpcodeName.SETLIST, (instr) => {SETLIST(instr); } },
+                {OpcodeName.CLOSE, (instr) => {CLOSE(instr); } },
+                {OpcodeName.CLOSURE, (instr) => {CLOSURE(instr); } },
+                {OpcodeName.VARARG, (instr) => {VARARG(instr); } },
             };
         }
         
-        public void Tick()
+        public void Decode()
         {
-
+            int num = 0;
         }
 
         // OpCodes
+
+        #region OpcodeHandlers
         private void MOVE(LuaInstruction instruction)
         {
-
+            this.Stack[this.Registers.A] = this.Stack[this.Registers.B];
+            this.IP++;
         }
 
         private void LOADK(LuaInstruction instruction)
         {
-
+            this.Stack[this.Registers.A] = this.Constants[this.Registers.Bx];
+            this.IP++;
         }
 
         private void LOADBOOL(LuaInstruction instruction)
         {
+            byte val = 1;
+            if (this.Registers.B == 0)
+                val = 0;
 
+            this.Registers.A = val;
+
+            if (this.Registers.C != 0)
+                this.IP++;
+            this.IP++;
         }
 
         private void LOADNIL(LuaInstruction instruction)
         {
-
+            for (int i = this.Registers.A; i < this.Registers.A + this.Registers.B; i++)
+                if (this.Stack.ContainsKey(i))
+                    this.Stack.Remove(i);
+            this.IP++;
         }
 
         private void GETUPVAL(LuaInstruction instruction)
         {
-
+            this.Stack[this.Registers.A] = this.Upvalues[this.Registers.B];
+            this.IP++;
         }
 
         private void GETGLOBAL(LuaInstruction instruction)
         {
-
+            int k = (int)this.Constants[instruction.Bx];
+            this.Stack[instruction.A] = this.Environment[k];
+            this.IP++;
         }
 
         private void GETTABLE(LuaInstruction instruction)
         {
-
+            //bool C = instruction.C > 0xFF && this.Constants[instruction.C-0xFF]
         }
 
         private void SETGLOBAL(LuaInstruction instruction)
         {
-
+            var k = (int)this.Constants[instruction.Bx];
+            this.Environment[k] = this.Stack[instruction.A];
+            this.IP++;
         }
 
         private void SETUPVAL(LuaInstruction instruction)
         {
-
+            this.Upvalues[instruction.B] = this.Stack[instruction.A];
+            this.IP++;
         }
 
         private void SETTABLE(LuaInstruction instruction)
         {
-
+            //bool B = instruction.B > 0xFF && this.Constants.ContainsKey(instruction.B - 0x100) || this.Stack[instruction.B];
+            //bool C = instruction.C > 0xFF && this.Constants.ContainsKey(instruction.C - 0x100) || this.Stack[instruction.C];
         }
 
         private void NEWTABLE(LuaInstruction instruction)
         {
-
+            this.Stack[instruction.A] = new Dictionary<int, object>();
         }
 
         private void SELF(LuaInstruction instruction)
@@ -234,30 +284,34 @@ namespace LuaSharpVM
         {
 
         }
+        #endregion OpcodeHandlers
+
 
         // Helpers
+
+        #region Helpers
         private byte get_byte()
         {
-            this.Index++;
-            return this.Buffer[this.Index - 1];
+            this.IP++;
+            return this.Buffer[this.IP - 1];
         }
 
         private int get_int()
         {
-            this.Index += 4;
-            return BitConverter.ToInt32(this.Buffer, this.Index - 4);
+            this.IP += 4;
+            return BitConverter.ToInt32(this.Buffer, this.IP - 4);
         }
 
         private float get_float()
         {
-            this.Index += 4;
-            return BitConverter.ToSingle(this.Buffer, this.Index - 4);
+            this.IP += 4;
+            return BitConverter.ToSingle(this.Buffer, this.IP - 4);
         }
 
         private long get_long()
         {
-            this.Index += 8;
-            return BitConverter.ToInt64(this.Buffer, this.Index - 8);
+            this.IP += 8;
+            return BitConverter.ToInt64(this.Buffer, this.IP - 8);
         }
 
         private string get_string(byte len = 0)
@@ -267,9 +321,10 @@ namespace LuaSharpVM
             string str = "";
             for(int i = 0; i < len; i++)
                 str += (char)this.Buffer[i];
-            this.Index += len;
+            this.IP += len;
             return str;
         }
+        #endregion
 
     }
 }
