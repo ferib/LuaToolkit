356c4d27c0da004684304007590221476e50be12
= <lopke123@live.nl>
2021-03-07 00:39:38 +0100

    add Web demo

---
+++ Web/Nancy/Webhost.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Nancy;
+using Nancy.ErrorHandling;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+
+namespace Web.Nancy
+{
+    public class Webhost : NancyModule
+    {
+        public Webhost()
+        {
+            Get("/", async x =>
+            {
+                return View["public/index.html"];
+            });
+
+            Post("/api/test", async x =>
+            {
+                return "TODO";
+            });
+        }
+    }
+
+#if !DEBUG
+    public class MyStatusHandler : IStatusCodeHandler
+    {
+        public bool HandlesStatusCode(global::Nancy.HttpStatusCode statusCode, NancyContext context)
+        {
+            return true;
+        }
+
+        public void Handle(global::Nancy.HttpStatusCode statusCode, NancyContext context)
+        {
+            return;
+        }
+    }
+#endif
+
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
+        {
+            // CORS Enabled
+            pipelines.AfterRequest.AddItemToEndOfPipeline((ctx) =>
+            {
+                ctx.Response.WithHeader("Access-Control-Allow-Origin", "*")
+                    .WithHeader("Access-Control-Allow-Methods", "POST,GET")
+                    .WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type")
+                    .WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type");
+            });
+        }
+    }
+}
--- Web/Program.cs
+++ Web/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Web.Nancy;
+using Nancy.Hosting.Self;
+using System.Threading;
 
 namespace Web
 {
@@ -6,7 +9,10 @@ namespace Web
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Launching Nancy on 127.0.0.1:9934");
+            NancyHost host = new NancyHost(new Uri("http://127.0.0.1:9934/"));
+            host.Start();
+            Thread.Sleep(-1);
         }
     }
 }
